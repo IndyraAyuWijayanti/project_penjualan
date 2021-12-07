b88f3dd32d013656fff4f77c83f83b98db2dae26
DeniHidayatullah <31883814+DeniHidayatullah@users.noreply.github.com>
2021-12-07 10:55:58 +0700

    config

--- Pemasaran_Puslitkoka/application/config/config.php
+++ Pemasaran_Puslitkoka/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/project_penjualan/Pemasaran_Puslitkoka';
+$config['base_url'] = 'http://localhost/Pemasaran_Puslitkoka';
 
 /*
 |--------------------------------------------------------------------------
