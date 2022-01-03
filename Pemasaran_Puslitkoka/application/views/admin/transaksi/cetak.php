7fab703c82afa03c749db0c373c2ae9c9871ca47
DeniHidayatullah <31883814+DeniHidayatullah@users.noreply.github.com>
2022-01-03 10:29:00 +0700

    cetak update

--- Pemasaran_Puslitkoka/application/views/admin/transaksi/cetak.php
+++ Pemasaran_Puslitkoka/application/views/admin/transaksi/cetak.php
@@ -92,7 +92,10 @@
             <tbody>
                 <?php foreach($tgltransaksi as $tgltransaksi){ ?>
                 <tr>
-                    <td class="td1">Pusat Penelitian Kopi dan Kakao Indonesia<br>Jl. PB. Sudirman 90. Jember</td>
+                    <td><img src="<?= base_url() ?>assets/admin/dist/img/puslit.png" width="30px"></td>
+                    <td class="td1"> Pusat
+                        Penelitian Kopi dan
+                        Kakao Indonesia<br>Jl. PB. Sudirman 90. Jember</td>
                     <td class="td1">Surat Perintah<br>Penyerahan Barang (DO)</td>
                     <td class="td1">No. DO
                         <?php echo $tgltransaksi->kode_transaksi ?><br>Tgl.
@@ -139,8 +142,48 @@
             <?php $i++; } ?>
             <tfoot>
                 <?php foreach($pembayaran as $transaksii){ ?>
+
+                <?php 
+                function penyebut($nilai) {
+                $nilai = abs($nilai);
+                $huruf = array("", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas");
+                $temp = "";
+                if ($nilai < 12) {
+                    $temp = " ". $huruf[$nilai];
+                } else if ($nilai <20) {
+                    $temp = penyebut($nilai - 10). " Belas";
+                } else if ($nilai < 100) {
+                    $temp = penyebut($nilai/10)." Puluh". penyebut($nilai % 10);
+                } else if ($nilai < 200) {
+                    $temp = " Seratus" . penyebut($nilai - 100);
+                } else if ($nilai < 1000) {
+                    $temp = penyebut($nilai/100) . " Ratus" . penyebut($nilai % 100);
+                } else if ($nilai < 2000) {
+                    $temp = " Seribu" . penyebut($nilai - 1000);
+                } else if ($nilai < 1000000) {
+                    $temp = penyebut($nilai/1000) . " Ribu" . penyebut($nilai % 1000);
+                } else if ($nilai < 1000000000) {
+                    $temp = penyebut($nilai/1000000) . " Juta" . penyebut($nilai % 1000000);
+                } else if ($nilai < 1000000000000) {
+                    $temp = penyebut($nilai/1000000000) . " Milyar" . penyebut(fmod($nilai,1000000000));
+                } else if ($nilai < 1000000000000000) {
+                    $temp = penyebut($nilai/1000000000000) . " Trilyun" . penyebut(fmod($nilai,1000000000000));
+                }     
+                return $temp;
+                }
+
+                function terbilang($nilai) {
+                if($nilai<0) {
+                    $hasil = "Minus ". trim(penyebut($nilai));
+                } else {
+                    $hasil = trim(penyebut($nilai));
+                }     		
+                return $hasil;
+                }
+                $angka = $transaksi->total;
+                ?>
                 <tr>
-                    <td class="td">Biaya pengiriman<br>Terbilang : <?=  ' Rupiah';?> </td>
+                    <td class="td">Biaya pengiriman<br>Terbilang : <?=  terbilang($angka).' Rupiah';?> </td>
                     <td colspan='3'></td>
                     <td class="td">
                         <?php echo 'Rp'.number_format($transaksii->ongkir) ?><br>
